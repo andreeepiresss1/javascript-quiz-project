8fd5ca2187eff8f12c76f319dd978a219f5630db
Andre Pires <andreeepiresss1@gmail.com>
2025-09-18 20:22:56 +0100

    Day 1 is done

--- src/question.js
+++ src/question.js
@@ -2,6 +2,75 @@ class Question {
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
-
+    constructor(text, choices, answer, difficulty){
+        this.text=text;
+        this.choices=choices;
+        this.answer=answer;
+        this.difficulty=difficulty;
+    }
     // 2. shuffleChoices()
+    shuffleChoices(){
+        const oldArray = [...this.choices];
+        const newArray=[];
+        while (oldArray.length>0){
+            const randomArrayIndex = Math.floor(Math.random()*(oldArray.length));
+            if(oldArray[randomArrayIndex]!==undefined){
+                newArray.push(oldArray[randomArrayIndex]);
+                oldArray.splice(randomArrayIndex, 1);
+            }
+        }
+        this.choices = newArray;
+        return this.choices;
+    
+    // shuffleChoices(){
+    //     let i = 10;
+    //     while (i>0){
+    //         const randomArrayIndex = Math.floor(Math.random()*(this.choices.length));
+    //         const pichOneAnswer = this.choices[randomArrayIndex];
+    //         this.choices.splice(randomArrayIndex, 1);
+    //         this.choices.push(pichOneAnswer);
+    //         i --;
+    //     }
+    // }
+    
+    }
 }
+
+
+
+// ------------------First Try-------------------------------
+
+
+// ------------------Testing-------------------------------
+// const testArray=["a","b","c","d"];
+// const newArray=[];
+// // const randomArrayIndex = Math.floor(Math.random()*(testArray.length));
+// // console.log(Math.floor(Math.random()*(testArray.length)));
+// // console.log(testArray[randomArrayIndex]);
+
+
+// ------------------First Try-------------------------------
+// while (testArray.length>0){
+//     const randomArrayIndex = Math.floor(Math.random()*(testArray.length));
+//     if(testArray[randomArrayIndex]!==undefined){
+//         newArray.push(testArray[randomArrayIndex]);
+//         testArray.splice(randomArrayIndex, 1);
+//     }
+// }
+
+// console.log(testArray);
+// console.log(newArray);
+
+
+// ------------------Second Try-------------------------------
+// const testArray2=["a","b","c","d"];
+// let i = 10;
+// while (i>0){
+//     const randomArrayIndex = Math.floor(Math.random()*(testArray2.length));
+//     const string = testArray2[randomArrayIndex];
+//     testArray2.splice(randomArrayIndex, 1);
+//     testArray2.push(string);
+//     i --;
+// }
+
+// console.log(testArray2);
--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,63 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining){
+        this.questions=questions;
+        this.timeLimit=timeLimit;
+        this.timeRemaining=timeRemaining;
+        this.correctAnswers=0;
+        this.currentQuestionIndex=0;
+    }
+
 
     // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }
+/*
+    Questions = [
+        {text:, choices:[], answer:[], difficulty:};
+        {text:, choices:[], answer:[], difficulty:};
+        {text:, choices:[], answer:[], difficulty:};
+        {text:, choices:[], answer:[], difficulty:}
+    ]
+*/
 
-    // 4. shuffleQuestions()
+
+
+    // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+       this.currentQuestionIndex +=1;
+    }
+
+
+    // 4. shuffleQuestions() 
+    shuffleQuestions(){
+        let i = 10;
+        while (i>0){
+            const randomArrayIndex = Math.floor(Math.random()*(this.questions.length));
+            const pichOneAnswer = this.questions[randomArrayIndex];
+            this.questions.splice(randomArrayIndex, 1);
+            this.questions.push(pichOneAnswer);
+            i --;
+        }
+    }
 
     // 5. checkAnswer(answer)
+    checkAnswer(answer){
+        const currentQuestion = this.getQuestion();
+        if(answer === currentQuestion.answer){
+            this.correctAnswers += 1;
+        }
+    }
+
 
     // 6. hasEnded()
+    hasEnded(){
+        if(this.currentQuestionIndex===this.questions.length){
+        return true;            
+        } else {
+            return false;
+        }
+    }
 }
